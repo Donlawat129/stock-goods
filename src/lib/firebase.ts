61158178c38810c9099e6734636243063d358f67
sarawut <sarawutsyndtech1@gmail.com>
2025-09-11 10:42:43 +0700

    แก้นิดหน่อยย

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
+
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FB_API_KEY,
   authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
