025eb027cb92eefcd6fa50edf7489e05f9bee548
kany5767 <108851569+kany5767@users.noreply.github.com>
2025-09-12 11:10:45 +0700

    ลองง

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter as Router } from 'react-router-dom'  // 👈 เพิ่มบรรทัดนี้
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router>   {/* 👈 ห่อ App ด้วย Router */}
+      <App />
+    </Router>
   </StrictMode>,
 )
