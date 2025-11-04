9ec45807375baeb382188b2df152ebb987ef5e7e
Donlawat Sasawattecha <donlawat.sas@gmail.com>
2025-11-04 11:12:48 +0700

    Change import to use type for ReactNode

--- src/components/common/RouteGuards.tsx
+++ src/components/common/RouteGuards.tsx
@@ -1,5 +1,5 @@
 // src/components/common/RouteGuards.tsx
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { useAuthUser } from "../../hooks/useAuthUser";
 
